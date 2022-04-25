2fc9c80ac024efe58d6c275128443e89150db03a
SnowDrive <luc.moralesr@gmail.com>
2022-04-25 13:27:27 -0400

    Change grid template columns calculation

--- src/components/Main/Highlights/Highlights.styles.js
+++ src/components/Main/Highlights/Highlights.styles.js
@@ -4,7 +4,7 @@ export const Highlights = styled.section`
   display: grid;
   width: 100%;
   gap: 3rem;
-  grid-template-columns: repeat(auto-fit, minmax(max(30rem, 100% / 2 - 4rem), 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(max(30rem, 100% / 2 - 3rem), 1fr));
 `
 
 export const H2 = styled.h2`
