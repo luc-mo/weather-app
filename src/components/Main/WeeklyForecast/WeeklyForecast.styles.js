89a30970ba896408ba20ca67f43db841e26ca879
SnowDrive <luc.moralesr@gmail.com>
2022-04-25 13:30:46 -0400

    Reduce grid gap

--- src/components/Main/WeeklyForecast/WeeklyForecast.styles.js
+++ src/components/Main/WeeklyForecast/WeeklyForecast.styles.js
@@ -4,6 +4,6 @@ export const WeeklyForecast = styled.section`
   display: grid;
   width: 100%;
   margin-bottom: 4rem;
-  gap: 4rem;
+  gap: 3rem;
   grid-template-columns: repeat(auto-fit, minmax(18rem, 1fr));
 `
